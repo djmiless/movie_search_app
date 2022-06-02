40721c9b00a850b5da0455afb37a39da6d504d2a
djmiless <oawotokun@gmail.com>
2022-06-02 10:58:31 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import './App.css';
 import {  useState, useRef } from 'react';
 import axios from 'axios';
 import * as ReactBootStrap from 'react-bootstrap'
-import Greetings from './Components/Greetings/Greetings';
+
+
+
+
+ 
+
 
 function App() {
 
@@ -11,13 +16,18 @@ function App() {
   const [movie, setMovie] = useState([])
   const [loading, setLoading] = useState(false)
 
-  let time_of_day;
-  let date = new Date();
-  if(date.getHours() >= 12){
-    time_of_day = "PM"
+  //get cart items from localStorage
+  let collectionStorage = localStorage.getItem("collection-storage");
+
+  if(collectionStorage == null){
+    collectionStorage = [];
   }else{
-    time_of_day = "AM"
+    collectionStorage = JSON.parse(collectionStorage);
   }
+
+  const [currentCollection, setCollection] = useState(collectionStorage);
+  const [currentTracker, setTracker ] = useState(0);
+
 
 
 
@@ -29,7 +39,7 @@ function App() {
       setLoading(true)
 
       axios.get(`http://www.omdbapi.com/?s=${currentValue}&apikey=283b9738`).then((feedback) => {
-        console.log(feedback)
+        // console.log(feedback)
         setMovie(feedback.data.Search)
         setLoading(false)
       })
@@ -38,7 +48,7 @@ function App() {
   }
 
   function loadMovie(index){
-    console.log(movie[index])
+    // console.log(movie[index])
 
     let single_movie = []
     single_movie.push(movie[index])
@@ -47,26 +57,82 @@ function App() {
     
   }
 
+  // let localStorePush
+  // let cartMovie = []
+  // function cartClick(index){
+  //   console.log(movie[index])
+  //   /// console.log(cartMovie.push(movie[index]))
+  //   localStorePush = cartMovie.push(movie[index])
+  //  localStorePush = localStorage.setItem('carter', JSON.stringify(cartMovie))
+  
+  // }
+
+  function loadCart (index){
+    
+    
+  }
+
+  function addToCollection(index){
+      //add this movie to collection
+      if(currentCollection.length == 0){
+        //there is nothing in the collection
+
+        const collectionStorage = [];
+        collectionStorage.push(movie[index]);
+
+        localStorage.setItem("collection-storage", JSON.stringify(collectionStorage));
+        
+        setCollection(collectionStorage);
+
+        
+
+        //save the collection to localStorage
+        
+      }else{
+        const collectionStorage = JSON.parse(localStorage.getItem("collection-storage"));
+
+        //check if this movie has been added already..
+
+        collectionStorage.push(movie[index]);
+
+        localStorage.setItem("collection-storage", JSON.stringify(collectionStorage));
+
+        setCollection(collectionStorage);
+
+
+        
+
+      }
+
+
+  }
+
+
+
+
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-        <a className="navbar-brand" href="#">Movie App</a>
+        <a className="navbar-brand" href="#">Movie Search App</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
+            <a className="nav-link active" aria-current="page" href="#"></a>
           </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
+              <a className="nav-link" href="#"></a>
             </li>
             
         </ul>
+        
+        <div className='cart' onClick={() => loadCart()}>
+          <span><i className="fas fa-cart-plus"></i><sup><small> {currentCollection.length}</small></sup></span>
+        </div>
         <form className="d-flex">
-          <Greetings whatever={time_of_day} another='hello'/>
           <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" ref={refer}/>
           <button className="btn btn-outline-success" type="button" onClick={getMovie}>Search</button>
         </form>
@@ -80,6 +146,33 @@ function App() {
           {
             movie.map((value, index) => {
               console.log("All movies: ", movie)
+              //check the localStorage
+              collectionStorage = localStorage.getItem("collection-storage");
+              let addOrRemoveBtn;
+              if(collectionStorage == null){
+                  //there is nothing in the collection
+                  addOrRemoveBtn = <button className='m-2 butt' onClick={() => addToCollection(index)}>Add to Collections</button>
+              }else{
+                collectionStorage = JSON.parse(collectionStorage);
+                const matchesFound = [];
+                for(let i = 0; i < collectionStorage.length; i++){
+                    if(collectionStorage[i]['imdbID'] == value.imdbID ){
+                      //we have found a match
+                      matchesFound.push(value);
+                    }
+
+                }
+
+                if(matchesFound.length > 0){
+                  addOrRemoveBtn = <button className='m-2 butt'>Remove from Collections</button>
+                }else{
+                  addOrRemoveBtn = <button className='m-2 butt' onClick={() => addToCollection(index)}>Add to Collections</button>
+                }
+
+              }
+
+
+
               return (
               <div className='col-3'key={index}>
                 <div className="card image-container" style={{width: "18rem"}}>
@@ -87,8 +180,8 @@ function App() {
                   <div className="card-body">
                     <h3 className="card-title">{value.Year}</h3>
                     <h4 className="card-text"> {value.Title} </h4>
-                    <button onClick={() => loadMovie(index)}>More</button>
-                    <button >Add to Collections</button>
+                    <button onClick={() => loadMovie(index)} className='butt'>More</button>
+                    {addOrRemoveBtn}
                   </div>
                 </div>
               </div>
@@ -97,7 +190,6 @@ function App() {
           }
         </div>: <div style={{color: 'red' }}>No Search result</div>}
       </div>
-
     </>
   );
 }
