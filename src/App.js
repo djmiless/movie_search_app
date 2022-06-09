62ee96d6e4d9ec6f5c7aa1048a54b7d38d8a5aef
djmiless <oawotokun@gmail.com>
2022-06-09 17:30:33 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,8 @@ import './App.css';
 import {  useState, useRef } from 'react';
 import axios from 'axios';
 import * as ReactBootStrap from 'react-bootstrap'
-
-
-
-
- 
-
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
 
@@ -26,10 +22,8 @@ function App() {
   }
 
   const [currentCollection, setCollection] = useState(collectionStorage);
-  const [currentTracker, setTracker ] = useState(0);
 
-
-
+ 
 
   const getMovie = (e) => {
     e.preventDefault();
@@ -57,19 +51,24 @@ function App() {
     
   }
 
-
-  function loadCart (index){
-  }
-
-
-
-  function addToCollection(index){
+  function addToCollection(imdbID){
       //add this movie to collection
+
+      console.log("Movies: ", movie)
       if(currentCollection.length == 0){
         //there is nothing in the collection
 
+        console.log("Console IMDB: ", imdbID)
         const collectionStorage = [];
-        collectionStorage.push(movie[index]);
+        //look for the movie with this imdbId from movie
+        for(let j = 0; j < movie.length; j++){
+          if(movie[j].imdbID == imdbID){
+            collectionStorage.push(movie[j])
+          }
+        }
+
+        
+        //collectionStorage.push(movie);
 
         localStorage.setItem("collection-storage", JSON.stringify(collectionStorage));
         
@@ -81,8 +80,13 @@ function App() {
         const collectionStorage = JSON.parse(localStorage.getItem("collection-storage"));
 
         //check if this movie has been added already..
+        for(let j = 0; j < movie.length; j++){
+          if(movie[j].imdbID == imdbID){
+            collectionStorage.push(movie[j])
+          }
+        }
 
-        collectionStorage.push(movie[index]);
+        //collectionStorage.push(movie[index]);
 
         localStorage.setItem("collection-storage", JSON.stringify(collectionStorage));
 
@@ -90,24 +94,64 @@ function App() {
       }
   }
 
-  function removeFromCollection(index){
-    let collectionStorage = JSON.parse(localStorage.getItem("collection-storage"));
-      for (let i = 0; i < collectionStorage.length; i++){
-       if(currentCollection.length){
-        console.log(collectionStorage)
-       } 
+  function removeFromCollection(imdbID){
+    //loop through the collections for a movie with a particular IMDB
+    console.log(currentCollection)
+
+    let collectionStorage = localStorage.getItem("collection-storage");
+    if(collectionStorage == null){
+
+    }else{
+      collectionStorage = JSON.parse(collectionStorage);
+
+      for(let i = 0; i< collectionStorage.length; i++){
+        if(collectionStorage[i].imdbID == imdbID){
+            collectionStorage.splice(i, 1);
+            break;
+        }
       }
 
+      localStorage.setItem("collection-storage", JSON.stringify(collectionStorage));
+      setCollection(collectionStorage);
+
+    }
+  }
+
+  
+  
+  function loadCollection(){
+    // alert("works")
+    let collectionStorage = localStorage.getItem("collection-storage");
+    if(collectionStorage != null){
+        collectionStorage = JSON.parse(collectionStorage);
+
+        setMovie(collectionStorage);
+    }
+  
   }
 
 
+  // to load login
 
+  const  [login, setLogin] = useState()
+
+  function loadLogin(){
+    //alert("works")
+    setLogin('login')
+  }
+
+  function loadRegister(){
+    //alert("works too")
+    setLogin('register')
+    setLogin()
+  }
+  
 
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-        <a className="navbar-brand" href="#">Movie Search App</a>
+        <a className="navbar-brand" href="#">Movie Searcher </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -122,7 +166,7 @@ function App() {
             
         </ul>
         
-        <div className='cart' onClick={() => loadCart()}>
+        <div className='cart' onClick={() => loadCollection()}>
           <span><i className="fas fa-cart-plus"></i><sup><small> {currentCollection.length}</small></sup></span>
         </div>
         <form className="d-flex">
@@ -133,6 +177,7 @@ function App() {
         </div>
       </nav>
       
+      
       <div className='container my-3' >
         {loading ? <ReactBootStrap.Spinner animation="border" style={{marginLeft: "45%"}}/>: <div></div>}
         {movie ? <div className='row'>
@@ -140,14 +185,18 @@ function App() {
             movie.map((value, index) => {
               // console.log("All movies: ", movie)
               //check the localStorage
+              console.log("Value: ", value)
               collectionStorage = localStorage.getItem("collection-storage");
               let addOrRemoveBtn;
               if(collectionStorage == null){
                   //there is nothing in the collection
-                  addOrRemoveBtn = <button className='m-2 butt' onClick={() => addToCollection(index)}>Add to Collections</button>
+                  addOrRemoveBtn = <button className='m-2 butt' onClick={() => addToCollection(value.imdbID)}>Add to Collections</button>
               }else{
                 collectionStorage = JSON.parse(collectionStorage);
                 const matchesFound = [];
+
+                console.log('Collection Storage: ', collectionStorage);
+
                 for(let i = 0; i < collectionStorage.length; i++){
                     if(collectionStorage[i]['imdbID'] == value.imdbID ){
                       //we have found a match
@@ -157,15 +206,12 @@ function App() {
                 }
 
                 if(matchesFound.length > 0){
-                  addOrRemoveBtn = <button className='m-2 butt' onClick={() => removeFromCollection(index)}>Remove from Collections</button>
+                  addOrRemoveBtn = <button className='m-2 butt' onClick={() => removeFromCollection(value.imdbID)}>Remove from Collections</button>
                 }else{
-                  addOrRemoveBtn = <button className='m-2 butt' onClick={() => addToCollection(index)}>Add to Collections</button>
+                  addOrRemoveBtn = <button className='m-2 butt' onClick={() => addToCollection(value.imdbID)}>Add to Collections</button>
                 }
 
               }
-
-
-
               return (
               <div className='col-3'key={index}>
                 <div className="card image-container" style={{width: "18rem"}}>
@@ -183,7 +229,15 @@ function App() {
           }
         </div>: <div style={{color: 'red' }}>No Search result</div>}
       </div>
-    </>
+
+      
+      {login ? <Login loadRegister={loadRegister}/> : <Register loadLogin={loadLogin}/>}
+      
+
+      
+      
+
+  </>
   );
 }
 
